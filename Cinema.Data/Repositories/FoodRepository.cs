0eb824c14f38e9198edb454aff5a6c4e228af58e
DESKTOP-L31ROEQ\JackQ <9103nguyentrongcanhnh1112@gmail.com>
2018-04-23 15:48:58 +0700

    bai9

---
+++ Cinema.Data/Repositories/FoodRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cinema.Data.Infastructure;
+using Cinema.Model.Models;
+
+namespace Cinema.Data.Repositories
+{
+    public interface IFoodRepository : IRepository<Food>
+    {
+    }
+
+    public class FoodRepository : RepositoryBase<Food>, IFoodRepository
+    {
+        public FoodRepository(IDbFactory DbFactory) : base(DbFactory)
+        {
+        } 
+    }
+}
---
+++ Cinema.Model/Models/Food.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.Model.Models
+{
+    [Table("Foods")]
+    public class Food
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FoodID { set; get; } 
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { set; get; }
+
+        [MaxLength(100)]
+        public string Images { set; get; }
+
+        [Required]
+        public int Price { set; get; }
+
+        [MaxLength(100)]
+        public string Decription { set; get; }
+
+        public int InventoryItem { set; get; }
+
+        public virtual IEnumerable<OrderDetail> OrderDetails { set; get; }
+    }
+}
